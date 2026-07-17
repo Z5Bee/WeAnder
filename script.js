06fbc36de77c580d575ed006707a843286a4df95
Zalean Beebe <zalean.beebe22@gmail.com>
2026-07-16 22:37:35 -0400

    Added JavaScript interaction

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("factButton");
+const fact = document.getElementById("funFact");
+
+button.addEventListener("click", function () {
+
+    if (fact.style.display === "none") {
+        fact.style.display = "block";
+    } else {
+        fact.style.display = "none";
+    }
+
+});
